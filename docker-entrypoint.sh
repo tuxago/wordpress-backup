d6e0f9e96f8c4c5bdc5e63b61608b7bdd095b56a
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2017-01-13 09:50:41 +0100

    Add DB_HOST env parameter to adapt to dynamic host from libnetwork

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,7 @@ then
   echo "Creating cron entry to start backup at: $BACKUP_TIME"
   # Note: Must use tabs with indented 'here' scripts.
   cat <<-EOF >> backup-cron
+    DB_HOST=$DB_HOST
 	MYSQL_ENV_MYSQL_USER=$MYSQL_ENV_MYSQL_USER
 	MYSQL_ENV_MYSQL_DATABASE=$MYSQL_ENV_MYSQL_DATABASE
 	MYSQL_ENV_MYSQL_PASSWORD=$MYSQL_ENV_MYSQL_PASSWORD
